ff8a3d2e5ed411519d6a5997dcbe572b6a447ec8
hyper <hyperskill@hyperskill.org>
2023-11-28 00:30:04 +0600

    Stage 3/6

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 package mealplanner;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -10,16 +8,16 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
     static int mealId = 0;
     public static void main(String[] args) throws SQLException {
-        String DB_URL = "jdbc:postgresql:meals_db";
+        String DB_URL = "jdbc:postgresql://localhost:5432/meals_db";
         String USER = "postgres";
         String PASS = "1111";
         Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
         connection.setAutoCommit(true);
 
-        String dropTableQuery = "DROP TABLE IF EXISTS meals; DROP TABLE IF EXISTS ingredients;";
-        PreparedStatement preparedStatement = connection.prepareStatement(dropTableQuery);
-        preparedStatement.executeUpdate();
-
+        //        String dropTableQuery = "DROP TABLE IF EXISTS meals; DROP TABLE IF EXISTS ingredients;";
+//        PreparedStatement preparedStatement = connection.prepareStatement(dropTableQuery);
+//        preparedStatement.executeUpdate();
+        PreparedStatement preparedStatement;
         String createTablesQuery = "CREATE TABLE IF NOT EXISTS meals (" +
                 "category VARCHAR, " +
                 "meal VARCHAR, " +
@@ -33,39 +31,31 @@ public class Main {
         preparedStatement = connection.prepareStatement(createTablesQuery);
         preparedStatement.executeUpdate();
 
-//        String insertInfo = "INSERT INTO meals (category, meal, meal_id) VALUES (?, ?, ?)";
-//        preparedStatement = connection.prepareStatement(insertInfo);
-//        preparedStatement.setString(1, "Breakfast");
-//        preparedStatement.setString(2, "Oatmeal");
-//        preparedStatement.setInt(3, 1);
-//        preparedStatement.executeUpdate();
-//
-//        String selectAllContent = "SELECT * FROM meals";
-//        preparedStatement = connection.prepareStatement(selectAllContent);
-//        ResultSet allContents = preparedStatement.executeQuery();
-//        while (allContents.next()) {
-//            System.out.println(allContents.getString("category"));
-//            System.out.println(allContents.getString("meal"));
-//            System.out.println(allContents.getInt("meal_id"));
-//        }
+        String mealIdQuery = "SELECT meal_id FROM meals WHERE meal_id=(SELECT max(meal_id) FROM meals);";
+        preparedStatement = connection.prepareStatement(mealIdQuery);
+        ResultSet mealIdSql = preparedStatement.executeQuery();
+        int mealIdFromSqlToJava = 0;
+        while (mealIdSql.next()) {
+            mealIdFromSqlToJava = mealIdSql.getInt("meal_id");
+        }
+        if (mealIdFromSqlToJava > 0) {
+             mealId = mealIdFromSqlToJava;
+        } else {
+            mealId = 0;
+        }
 
         boolean isBye = false;
-        List<String> mealTypeList = new ArrayList<>();
-        List<String> mealNameList = new ArrayList<>();
-        List<String[]> ingredientsList = new ArrayList<>();
 
         while (!isBye) {
 
             System.out.println("What would you like to do (add, show, exit)?");
             String actionType = scanner.nextLine();
 
-            //Invalid input format
-            if (actionType.equals("show") && mealId == 0/*mealNameList.size() == 0*/) {
-                System.out.println("No meals saved. Add a meal first.");
-            } else if (!actionType.matches("add|show|exit")) {
-                continue;
-            }
-            //Invalid input format
+//            if (actionType.equals("show") && mealId == 0/*mealNameList.size() == 0*/) {
+//                System.out.println("No meals saved. Add a meal first.");
+//            } else if (!actionType.matches("add|show|exit")) {
+//                continue;
+//            }
 
             switch (actionType) {
                 case "exit" -> {
@@ -74,59 +64,28 @@ public class Main {
                     return;
                 }
                 case "add" -> {
-                    addMeal(/*mealTypeList, mealNameList, ingredientsList, */connection, preparedStatement);
+                    addMeal(connection, preparedStatement);
                 }
                 case "show" -> {
                     System.out.println();
-//                    for (int i = 0; i < mealTypeList.size(); i++) {
-//
-//                        System.out.println("Category: " + mealTypeList.get(i));
-//                        System.out.println("Name: " + mealNameList.get(i));
-//                        System.out.println("Ingredients:");
-//                        for (String s : ingredientsList.get(i)) {
-//
-//                            System.out.println(s);
-//                        }
+                    for (int j = 1; j <= mealId; j++) {
 
+                        String selectAllMealsCategories2 = String.format("SELECT * FROM meals WHERE meal_id = %d;", j);
+                        preparedStatement = connection.prepareStatement(selectAllMealsCategories2);
+                        ResultSet all = preparedStatement.executeQuery();
+                        all.next();
+                        System.out.println("Category: " + all.getString("category"));
+                        System.out.println("Name: " + all.getString("meal"));
+                        System.out.println("Ingredients:");
 
-//                        System.out.println("SQL meals:");
-//                        String selectQuery = "SELECT * FROM meals";
-//                        preparedStatement = connection.prepareStatement(selectQuery);
-//                        ResultSet allCategoriesMeals = preparedStatement.executeQuery();
-//                        while (allCategoriesMeals.next()) {
-//                            System.out.print("category: " + allCategoriesMeals.getString("category") + " ");
-//                            System.out.print("meal: " + allCategoriesMeals.getString("meal") + " ");
-//                            System.out.println("meal id: " + allCategoriesMeals.getInt("meal_id") + " ");
-//                        }
-//
-//                        System.out.println("SQL ingredients:");
-//                        String selectQueryIngredients = "SELECT * FROM ingredients";
-//                        preparedStatement = connection.prepareStatement(selectQueryIngredients);
-//                        ResultSet allIngredients = preparedStatement.executeQuery();
-//                        while (allIngredients.next()) {
-//                            System.out.print("ingredient: " + allIngredients.getString("ingredient") + " ");
-//                            System.out.print("ingredient id: " + allIngredients.getInt("ingredient_id") + " ");
-//                            System.out.println("meal id" + allIngredients.getInt("meal_id"));
-//                        }
-
-                        for (int j = 1; j <= mealId; j++) {
-
-                            String selectAllMealsCategories2 = String.format("SELECT * FROM meals WHERE meal_id = %d;", j);
-                            preparedStatement = connection.prepareStatement(selectAllMealsCategories2);
-                            ResultSet all = preparedStatement.executeQuery();
-                            all.next();
-                            System.out.println("Category: " + all.getString("category"));
-                            System.out.println("Name: " + all.getString("meal"));
-                            System.out.println("Ingredients:");
-
-                            String selectAllIngredients2 = String.format("SELECT * FROM ingredients WHERE meal_id = %d", j);
-                            preparedStatement = connection.prepareStatement(selectAllIngredients2);
-                            ResultSet allIngredients2 = preparedStatement.executeQuery();
-                            while (allIngredients2.next()) {
-                                System.out.println(allIngredients2.getString("ingredient"));
-                            }
-                            System.out.println();
+                        String selectAllIngredients2 = String.format("SELECT * FROM ingredients WHERE meal_id = %d", j);
+                        preparedStatement = connection.prepareStatement(selectAllIngredients2);
+                        ResultSet allIngredients2 = preparedStatement.executeQuery();
+                        while (allIngredients2.next()) {
+                            System.out.println(allIngredients2.getString("ingredient"));
                         }
+                        System.out.println();
+                    }
                     }
                 }
             }
@@ -135,8 +94,7 @@ public class Main {
         connection.close();
         }
 
-    private static void addMeal(/*List<String> mealTypeList, List<String> mealNameList,
-                                List<String[]> ingredientsList, */Connection connection, PreparedStatement preparedStatement) throws SQLException {
+    private static void addMeal(Connection connection, PreparedStatement preparedStatement) throws SQLException {
 
         System.out.println("Which meal do you want to add (breakfast, lunch, dinner)?");
         String mealType = null;
@@ -147,7 +105,6 @@ public class Main {
             }
             System.out.println("Wrong meal category! Choose from: breakfast, lunch, dinner.");
         }
-        //mealTypeList.add(mealType);
 
 
         System.out.println("Input the meal's name:");
@@ -159,7 +116,6 @@ public class Main {
             }
             System.out.println("Wrong format. Use letters only!");
         }
-        //mealNameList.add(mealName);
 
         System.out.println("Input the ingredients:");
         String ingredientsStr = null;
@@ -190,6 +146,5 @@ public class Main {
             preparedStatement.setInt(3, mealId);
             preparedStatement.executeUpdate();
         }
-        //ingredientsList.add(ingredientsArrFixed);
     }
 }
